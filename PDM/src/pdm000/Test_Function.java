befef43078e3fcdaf5a74ea9368bc060a4178401
andy_chuang <andy_chuang@.accton.com.tw>
2021-03-12 14:03:02 +0800

    20210312 測試給nina看

--- PDM/src/pdm000/Test_Function.java
+++ PDM/src/pdm000/Test_Function.java
@@ -8,16 +8,17 @@ import com.agile.px.ICustomAction;
 import com.sun.corba.se.spi.orbutil.fsm.Action;
 import com.sun.org.apache.bcel.internal.generic.AALOAD;
 
-public class Test_Function implements ICustomAction{
+public class Test_Function implements ICustomAction {
 
 	@Override
 	public ActionResult doAction(IAgileSession arg0, INode arg1, IDataObject arg2) {
 		// TODO Auto-generated method stub
 		Event_AutoChangeNumber pg1 = new Event_AutoChangeNumber();
 		Px_AutoApprover_check pg2 = new Px_AutoApprover_check();
-		//SYSO
-		return new ActionResult(ActionResult.STRING,pg2.testFunction());
+		// SYSO
+
+		System.out.println(123);
+		return new ActionResult(ActionResult.STRING, pg2.testFunction());
 	}
-	
 
 }
